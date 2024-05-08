2ab4fd561a1382faed212d465666723e5ed3523c
meyup <m.eypoglu@gmail.com>
2024-05-08 15:48:29 +0300

    update

---
+++ Practice_2_Box_Match/src/Practice_2_Box_Match.java
@@ -0,0 +1,9 @@
+public class Practice_2_Box_Match {
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Ring r = new Ring(marc,alex , 90 , 100);
+        r.run();
+
+    }
+}
